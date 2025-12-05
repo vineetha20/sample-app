0df6888bfa5afab88033bd7ae37b9c9e6720c1e0
vineetha20 <37927332+vineetha20@users.noreply.github.com>
2025-12-04 18:08:11 -0600

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,8 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        String apiKey = System.getenv("API_KEY");
+        String env = System.getenv("ENV");
+        String apiKey = System.getenv("API_KEY_DEV");
 
         if (apiKey != null && !apiKey.isEmpty()) {
             System.out.println("Secret retrieved successfully.");
